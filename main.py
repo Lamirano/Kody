6969e12fdbe914426e3adc3237803fc44cc512a2
Lamirano <102316202+Lamirano@users.noreply.github.com>
2022-03-24 18:18:06 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,79 +1,32 @@
-#Konzolová kalkulačka bez vytvorených funkcií. Vetvenie pomocou podmienok, riešenie kalkulačky riešené ľahko cez podmienky a inputy. 
-import math
+import time
 while True:
- 
-  
-  print ("Vítajte v programe kalkulačka\nPonuka:\n1 sčítanie\n2 odčítanie\n3 násobenie\n4 delenie\n5 n-ta mocnina\n6 n-ta odmocnina\n7 kvadratická rovnica\n8 Pytagorová veta\n9 Objem a obsah kvádra")
-  vstup=int(input("Zadajte svoju voľbu:"))
-  print ("Vaša voľba je :",vstup)
+  print('\x1bc')
+  print("Vítajte v menu prajeti si pokračovať ? Ak chcete pokračovať stlačte 1 ak nie tak 2")
+  vstup=int(input("Zadajte svoju voľbu :"))
   if vstup==1:
-    a=float(input("Zadaj prvé číslo:"))
-    b=float(input("Zadaj druhé číslo:"))
-    print("Sčítanie čísel ",a,"+",b,"=",a+b)
-  elif vstup==2:
-    a=float(input("Zadaj prvé číslo:"))
-    b=float(input("Zadaj druhé číslo:"))
-    print("Odčítanie čísel ",a,"-",b,"=",a-b)
-  elif vstup==3:
-    a=float(input("Zadaj prvé číslo:"))
-    b=float(input("Zadaj druhé číslo:"))
-    print("Násobenie čísel",a,"*",b,"=",a*b)
-  elif vstup==4:
-    a=float(input("Zadaj prvé číslo:"))
-    b=float(input("Zadaj druhé číslo:"))
-    if a==0:
-      print("Nulou sa nedá deliť")
-    else :
-      print("Delenie čísel ",a,"/",b,"=",a/b)
-  elif vstup==5:
-    a=float(input("Zadaj číslo :"))
-    n=float(input("Zadaj mocninu:"))
-    print(n,"-tá mocnicna čísla",a,"=",a**n)
-  elif vstup==6:
-    a=float(input("Zadaj číslo:"))
-    n=float(input("Zadaj odmocninu:"))
-    print(n,"-tá odmocnina čísla",a,"=",a**(1/n))
-  elif vstup==0:
-    print("Dovidenia !!!")
+    print("Nech sa vám páči")
+  else:
+    print("Dovidenia :)")
     exit()
-  elif vstup==7:
-    a=int(input("Zadaj hodnotu a :"))
-    b=int(input("Zadaj hodnotu b :"))
-    c=int(input("Zadaj hodnotu c :"))
-    print("Hodnoty sú a=",a,"b=",b,"c=",c)
-    D=b*b-4*a*c
-    print("Diskrimants sa rovná=",D)
-    if D<0 :
-      print("Rovnica nemá riešenie v R")
-    if D==0:
-      x=(-b)/2*a
-      print("x=",x)
-    if D>0 :
-      x1=((-b)+math.sqrt(D)/2*a)
-      x2=((-b)-math.sqrt(D)/2*a)
-      print("x1=",x1,"x2=",x2)
-  elif vstup==8:
-    a=float(input("Zadaj hodnotu a :"))
-    b=float(input("Zadaj hodnotu b :"))
-    c=math.sqrt((a**2)+(b**2))
-    print("Strana c je ",c,"\n\n")
-  elif vstup==9:
-    print("1.Kváder\n2.Kocka")
-    vstup2=int(input("Zadajte svoju voľbu "))
-    if vstup2==1:
-      a=float(input("Zadajte stranu a :"))
-      b= float(input("Zadajte stranu b :"))
-      c=float(input("Zadajte stranu c :"))
-      V=a*b*c
-      S=(2*a*b)+(2*b*c)+(2*a*c)
-      print("Objem kvádra je V=",V,"a obsah kvádra je S=",S)
-    if vstup2==2:
-      a=float(input("Zadajte stranu a :"))
-      if a==0:
-        print("Kocka so stranou 0 neexistuje")
-        exit()
-      V=a*a*a
-      S=6*a*a
-      print("Objem kocky je V=",V,"a obsah kocky je S=",S)
-      print('\x1bc')
-
+  vstup1=input("\nZadaj svoje meno :")
+  print("Vítajte",vstup1,"\nv programe na vypočet Vášho veku,ktorý Vám aj určí koľko rokov Vám zostáva do dôchodku,a do akej vekovej skupiny patríte")
+  print("Vyberte si",vstup1,"čo Vám môžem vypočítať \n1: Váš vek\n2: Koľko Vám zostáva do dôchodku\n3: Do akej vekovej kategorie patríte")
+  vstup2=int(input("Zadajte vašu voľbu :"))
+  print("\nVaša voľba je",vstup2)
+  if vstup2==1:
+    a=float(input("Zadajte Váš rok narodenia :"))
+    b=float(input("Zadajte aktualný rok :"))
+    print("Váš vek je",b-a,"rokov")
+  elif vstup2==2:
+    a=float(input("Váš aktualný vek "))
+    b=float(65)
+    print("Do dôchodku Vám zostáva",b-a,"rokov")
+  elif vstup2==3:
+    a=float(input("Zadajte svoj vek :"))
+    if a<0 or a<18:
+      print("Vy ste junior ")
+    elif a<50:
+      print("Ste v strednom veku ")
+    elif a>50: 
+      print("Vy ste senior ")
+  time.sleep(1)
